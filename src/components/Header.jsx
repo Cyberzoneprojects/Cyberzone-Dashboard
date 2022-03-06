4ca02076c922cb3a4d5439d3c2eb3a0b7136f4de
Cyberzoneprojects <cyberzone683@gmail.com>
2022-03-06 11:46:21 +0100

    making logo nav responsive

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,16 +5,15 @@ function Header(){
         <>
             <header className="navbar navbar-dark sticky-top bg- flex-md-nowrap px-0 py-0 shadow">
                 <div className='d-sm-flex justify-content-between bg-dark col-md-2 py-4'>
-                    <a className="navbar-brand col-md-3 col-lg-2 me-0 px-3 col-sm-" href="#">Logo</a>
-                    <button className="navbar-toggler position-absolute d-md-none collapsed col-sm-" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon "></span>
+                    <h2 className='text-white d-none d-md-block mx-auto'>Logo</h2>
+                    <button className="navbar-toggler d-md-none collapsed col-sm- " type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon bg-dark"></span>
                     </button>
                 </div>
                 {/* <input className="form-control form-control-dark w-100" type="text" placeholder="Search" aria-label="Search"/> */}
+                <a className="navbar-brand col-md-3 text-dark mx-auto d-lg-none d-md-none d-xl-none" href="#">Logo</a>
                 <div className="navbar-nav">
-                    <div className="nav-item text-nowrap">
-                    <a className="nav-link px-3" href="#">Sign out</a>
-                    </div>
+                <h2 className='text-black d-none d-md-block mx-auto' style={{paddingRight: '2rem'}}>LogOut</h2>
                 </div>
             </header>
         </>
