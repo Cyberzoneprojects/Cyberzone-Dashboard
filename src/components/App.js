856fce9ac72dd7885f189d2ba6d14f002d871b4e
Cyberzoneprojects <cyberzone683@gmail.com>
2022-03-07 20:44:03 +0100

    tester

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import Test from './sidebar/test'
+class App extends Component {
+  render() {
+    return (
+      <>
+        
+      <div className="app">
+        <Test />
+      </div></>
+    )
+  }
+}
+export default App
